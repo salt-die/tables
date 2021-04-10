c8018f35db8563d2b9330dcee247ecc3d25334ca
salt-die <53280662+salt-die@users.noreply.github.com>
2021-04-10 16:29:26 -0500

    `_next_all` returns immediately if `iterables` is empty.

--- tables/utils.py
+++ tables/utils.py
@@ -3,6 +3,9 @@ from functools import wraps
 def _next_all(*iterables):
     """Yield the next item in each iterable or raise an error if each iterable doesn't raise a `StopIteration`
     """
+    if not iterables:
+        return
+
     first, *rest =  iterables
     try:
         yield next(first)
